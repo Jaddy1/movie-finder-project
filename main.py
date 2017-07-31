63c47a2721424ecfe858e47d84a64de782a1b515
Demo User <demouser@MacBook-Air.local>
2017-07-31 16:12:40 -0400

    Home page MVP with Css

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ jinja_environment = jinja2.Environment(loader=
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/index.html')
+        template = jinja_environment.get_template('templates/search_zip.html')
         self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
