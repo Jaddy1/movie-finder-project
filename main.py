785d0e3526b157d668cc14c1efb584caae6853dc
Demo User <demouser@MacBook-Air.local>
2017-07-31 16:15:15 -0400

    Added API MVP version

--- main.py
+++ main.py
@@ -14,17 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 import jinja2
 import os
+import webapp2
+import json
+import urllib2
+import urllib
+import logging
 
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/index.html')
+        logging.info("hello from the get")
+        template = jinja_environment.get_template('templates/search_zip.html')
         self.response.write(template.render())
+    def post(self):
+        logging.info("hello")
+        template = jinja_environment.get_template('templates/movie-showings.html')
+        zip_search = self.request.get('zip_code_input')
+
+        base_url = "http://data.tmsapi.com/v1.1/movies/showings?"
+        url_params = {'zip': zip_search, 'api_key': 'dev9tj3wfhmyq736p82tnffn', 'startDate': '2017-07-31'}
+        movie_response = urllib2.urlopen(base_url + urllib.urlencode(url_params)).read()
+        parsed_movie_dictionary = json.loads(movie_response)
+        logging.info(parsed_movie_dictionary)
+        first_movie = parsed_movie_dictionary[0]
+        
+        self.response.write(template.render(first_movie))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
